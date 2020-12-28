1b0e38b7c56b18c04fe7a3c590130f5e58a379c7
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-12-28 13:40:04 -0500

    Juju deportes judo

---
+++ JavaScripts/deporteJudo.js
@@ -0,0 +1,19 @@
+// Se declara la variable deporte que contiene los datos
+var deporte = {
+    nombre:"judo",
+    origen: "japon",
+    clubes:{
+      dojo:["Sensei VU", "Sensei Sata"],
+      ciudad:["Inmaculada", "Tokio"],
+    }
+}
+// se imprime en consola los resultados que contiene a variable deporte
+// concatenando los string con las propiedades y sus posiciones de esa variable
+console.log( `Club de ${deporte.nombre} ${deporte.clubes.dojo[0]} Ciudad:  ${deporte.clubes.ciudad[0]}`);
+// resultado de la consulta: Club de judo Sensei VU Ciudad:  Inmaculada
+
+// se alamacena el resultado en una variable mensaje con las primeras posiones del arreglo
+var mensaje = `Club de ${deporte.nombre} ${deporte.clubes.dojo[1]} Ciudad: ${deporte.clubes.ciudad[1]}`
+// se imprime la variable
+console.log(` "${mensaje}" `);
+// resultad de variable mensaje:  "Club de judo Sensei Sata Ciudad: Tokio"
