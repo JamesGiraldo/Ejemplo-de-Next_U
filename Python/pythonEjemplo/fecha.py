50c7a814d28650bf10e455bc997b3d7ce5e3ce05
jamesgiraldo <jamesgiiraldo@gmail.com>
2021-01-03 09:47:56 -0500

    fecha en python

---
+++ Python/pythonEjemplo/fecha.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+# nombreCripto=input("Nombre de la Criptomoneda: ")
+# cantCripto=float(input("Cantidad acumulada de la Criptomoneda: "))
+# cotizacion=float(input("Cotización por US$ del día de la Criptomoneda: "))
+# ahora = datetime.now()
+# print("La fecha completa y hora en la que obtuvo la información fue: " +ahora.strftime("%A %d/ de %B/ de %Y a las %I:%M:%S%p"))
+
+hora=datetime.now()
+aja = hora.strftime("%A %d/ de %B/ de %Y a las %I:%M:%S%p")
+
+ncripto = input("Nombre de tu criptomoneda: ")
+cantcripto = int(input("Cuántas criptomonedas tienes acumuladas? "))
+cotcripto = float(input("Coloca el valor en dólares de tu criptomoneda: "))
+
+total = (cantcripto * cotcripto)
+
+print("Usted tiene un total de $:", str(total),"/ Por sus:",str(cantcripto),
+str(ncripto),"/ al",str(aja))
