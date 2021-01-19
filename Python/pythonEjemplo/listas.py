631834d9444468153a3a2f09f5fc511588cfe170
jamesgiraldo <jamesgiiraldo@gmail.com>
2021-01-18 22:44:39 -0500

    Variables y listas.

--- Python/pythonEjemplo/listas.py
+++ Python/pythonEjemplo/listas.py
@@ -3,13 +3,13 @@ cant = []
 cotiz = []
 
 i = 0
-while i <5:
+while i < 5:
     cripto.append(input("Ingrese el nombre de la moneda: "))
     cant.append(float(input("Ingrese la cantidad de "+cripto[i]+": ")))
-    cotiz.append(float(input("Ingrese la cotización en USD de"+cripto[i]+":")))
+    cotiz.append(float(input("Ingrese la cotización en USD de "+cripto[i]+": ")))
     i = i + 1
 
 i = 0
-while i<5:
-    print("Moneda: "+cripto[i]+", cantidad: "+cant[i]+", precio en USD: "+cotiz[i])
+while i < 5:
+    print("Moneda: "+ str(cripto[i])+", cantidad: "+ str(cant[i])+", precio en USD: "+str(cotiz[i]))
     i = i + 1
--- Python/pythonEjemplo/variables.py
+++ Python/pythonEjemplo/variables.py
@@ -7,10 +7,10 @@ valorTotal = 0
 # solicitud de información al usuario
 nombreCripto = input("Nombre de la Criptomoneda: ")
 # imprimir el resultado en la variable nombreCripto
-print("Moneda: " + nombreCripto)
+print("La Moneda indicada es: " + nombreCripto)
 # solicitar nuevamente la información al usuario
-cantCripto = int(input("Cantidad acumulada de la criptomoneda: "))
-cotizacion = float(input("Cotización por US$ del día de la criptomoneda: "))
+cantCripto = int(input("Cantidad acumulada de la criptomoneda " + nombreCripto + ": "))
+cotizacion = float(input("Cotización por US$ del día de la criptomoneda "+ nombreCripto + ": "))
 # operación
 valorTotal = cantCripto * cotizacion
 # imprimir valor final
